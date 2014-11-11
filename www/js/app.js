1ff9e5265c25d7082743d87770cb50722e65ff29
anais <anais@anais-MS-16G7>
2014-11-11 10:53:39 +0100

    | Anais | modification index.html

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,28 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('forget-me-not', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                    .state('home', {
+                        url: '/',
+                        templateUrl: '/pages/home.html'
+                    });
+
+            $urlRouterProvider.otherwise('/');
+        });
+/*
+ .run(function ($ionicPlatform) {
+ $ionicPlatform.ready(function () {
+ // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+ // for form inputs)
+ if (window.cordova && window.cordova.plugins.Keyboard) {
+ cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+ }
+ if (window.StatusBar) {
+ StatusBar.styleDefault();
+ }
+ });
+ })
+ */
