ba09cfd28d9de3698e7ae703f7d09af6261d8535
Marjorie Dugrand <marjorie.dugrand@gmail.com>
2014-11-25 15:53:16 +0100

    |marjorie|work on add context|

---
+++ www/js/contexts.js
@@ -0,0 +1,65 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+fmnApp.controller("ContextController", function ($scope, userService, databaseService) {
+    
+    $scope.context = new Context();
+    
+    $scope.useAddress = false;
+    $scope.useGeolocation = false;
+    
+    $scope.addressChoiceChange = function() {
+        $scope.useAddress = !($scope.useAddress);
+        $scope.useGeolocation = false;
+    };
+    
+    $scope.geolocationChoiceChange = function() {
+        $scope.useGeolocation = !($scope.useGeolocation);
+        $scope.useAddress = false;
+        if($scope.useGeolocation) {
+            $scope.checkGeolocation();
+        }
+    };
+    
+    $scope.addressUsed = function() {
+        return $scope.useAddress && !($scope.useGeolocation);
+    };
+    
+    $scope.geolocationUsed = function() {
+        return !($scope.useAddress) && $scope.useGeolocation;
+    };
+    
+    $scope.saveContext = function() {
+
+        $scope.context.owner = userService.loadUser().username;
+        //$scope.context.lastModification = Date.now();
+	databaseService.storeContext($scope.context); 
+    };
+
+    $scope.cancelContext = function() {
+	$scope.context = new Context();
+    };
+    
+    $scope.geolocationMessage = '';
+    
+    $scope.checkGeolocation = function() {
+      if(userService.loadUser().geolocalization) {
+          if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition($scope.context.location);
+              $scope.geolocationMessage = 'Your location: ' + $scope.context.location;
+          } else {
+              $scope.geolocationMessage = "Geolocation is not supported by this browser.";
+          }
+      } else {
+          $scope.geolocationMessage = "Please modify your settings to accept geolocation.";
+      }  
+    };
+    
+    $scope.getGeolocationMessage = function() {
+        return $scope.geolocationMessage;
+    };
+ 
+});
